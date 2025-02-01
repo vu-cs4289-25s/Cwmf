83ff947785096cd9e831b2c03f2fb188fec85240
eric sang <ericr.sang@gmail.com>
2025-02-01 02:19:58 -0600

    input update

--- frontend/app/join/page.js
+++ frontend/app/join/page.js
@@ -38,7 +38,7 @@ export default function AccountPage() {
                                     type="code"
                                     required
                                     autoComplete="current-password"
-                                    className="block w-full rounded-md bg-grey-200 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                    className="block w-full rounded-md bg-gray-200 px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                 />
                             </div>
                         </div>
