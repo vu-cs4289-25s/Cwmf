ab03c992c955e3b18a4df60b8ee08a595c8d8003
eric sang <ericr.sang@gmail.com>
2025-02-19 12:37:18 -0600

    deleted frontend

--- frontend/.next/static/chunks/pages/index.js
+++
@@ -1,8 +0,0 @@
-__turbopack_load_page_chunks__("/", [
-  "static/chunks/node_modules_next_f80f03._.js",
-  "static/chunks/node_modules_react-dom_82bb97._.js",
-  "static/chunks/node_modules_1b7400._.js",
-  "static/chunks/[root of the server]__13f17c._.js",
-  "static/chunks/pages_index_5771e1._.js",
-  "static/chunks/pages_index_1bc7a6._.js"
-])
